5d699ee5911e1d37ca997e21bed8a1f1361c1801
Saif <113651806+saifs27@users.noreply.github.com>
2024-04-14 18:59:42 -0500

    minor edit

--- src/world.hpp
+++ src/world.hpp
@@ -9,7 +9,7 @@ namespace noether
 		Vector3 gravity = Vector3(0, -9.8, 0);
 
 	public:
-		void add_object(Object& object);
+	    void add_object(Object& object);
 		void remove_object(Object& object);
 		void update(float dt);
 	};
